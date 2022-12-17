2b03286336a7131945a4f77421b44217e0d16bb7
niu0217 <1033856427@qq.com>
2022-12-17 21:12:44 +0800

    二分法算法

--- test.cpp
+++ test.cpp
@@ -1,13 +1,40 @@
-/* ************************************************************************
-> File Name:     test.cpp
-> Author:        程序员niu0217
-> Created Time:  六 12/17 20:08:44 2022
-> Description:   
- ************************************************************************/
-#includes<iostream>
+#include<iostream>
+#include<vector>
 using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;//左闭右闭区间
+        int middle = 0;
+        while(left <= right){
+            middle = left + (right - left)/2;
+            if(nums[middle] > target) {
+                right = middle -1;
+            }
+            else if(nums[middle] < target) {
+                left = middle + 1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+};
 
 int main()
 {
-    cout<<"nihao  "<<endl;
+    Solution slu;
+    vector<int> a ={
+        1,4,7,9,10,24
+    };
+    int result = slu.search(a,9);
+    cout<<result<<endl;
 }
+
+
+
+
+
