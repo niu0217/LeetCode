352c6a6b71737c6f7e3776be4f1cae6c488841ea
niu0217 <1033856427@qq.com>
2022-12-17 20:10:32 +0800

    测试代码

---
+++ test.cpp
@@ -0,0 +1,13 @@
+/* ************************************************************************
+> File Name:     test.cpp
+> Author:        程序员niu0217
+> Created Time:  六 12/17 20:08:44 2022
+> Description:   
+ ************************************************************************/
+#includes<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"nihao  "<<endl;
+}
